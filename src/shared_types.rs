68c5d435607ae1a2a1b85b369be14763e0dd7a2d
Nikolaus Waxweiler <madigens@gmail.com>
2019-11-24 21:52:36 +0000

    De/serialize Guideline

--- src/shared_types.rs
+++ src/shared_types.rs
@@ -3,7 +3,7 @@ use std::fmt;
 use serde::de;
 use serde::de::{Deserializer, Visitor};
 use serde::ser;
-use serde::ser::Serializer;
+use serde::ser::{SerializeStruct, Serializer};
 use serde::{Deserialize, Serialize};
 
 #[cfg(feature = "druid")]
@@ -95,6 +95,83 @@ pub enum Line {
     /// An angled line passing through `(x, y)` at `degrees` degrees counteer-clockwise
     /// to the horizontal.
     Angle { x: f32, y: f32, degrees: f32 },
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+struct RawGuideline {
+    x: Option<f32>,
+    y: Option<f32>,
+    angle: Option<f32>,
+    name: Option<String>,
+    color: Option<Color>,
+    identifier: Option<Identifier>,
+}
+
+impl Serialize for Guideline {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let (x, y, angle) = match self.line {
+            Line::Vertical(x) => (Some(x), None, None),
+            Line::Horizontal(y) => (None, Some(y), None),
+            Line::Angle { x, y, degrees } => (Some(x), Some(y), Some(degrees)),
+        };
+
+        let mut guideline = serializer.serialize_struct("RawGuideline", 6)?;
+        guideline.serialize_field("x", &x)?;
+        guideline.serialize_field("y", &y)?;
+        guideline.serialize_field("angle", &angle)?;
+        guideline.serialize_field("name", &self.name)?;
+        guideline.serialize_field("color", &self.color)?;
+        guideline.serialize_field("identifier", &self.identifier)?;
+        guideline.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for Guideline {
+    fn deserialize<D>(deserializer: D) -> Result<Guideline, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let guideline = RawGuideline::deserialize(deserializer)?;
+
+        let x = guideline.x;
+        let y = guideline.y;
+        let angle = guideline.angle;
+
+        let line = match (x, y, angle) {
+            (Some(x), None, None) => Line::Vertical(x),
+            (None, Some(y), None) => Line::Horizontal(y),
+            (Some(x), Some(y), Some(degrees)) => {
+                if !(0.0..=360.0).contains(&degrees) {
+                    return Err(de::Error::custom("angle must be between 0 and 360 degrees."));
+                }
+                Line::Angle { x, y, degrees }
+            }
+            (None, None, _) => {
+                return Err(de::Error::custom("x or y must be present in a guideline."))
+            }
+            (None, Some(_), Some(_)) | (Some(_), None, Some(_)) => {
+                return Err(de::Error::custom(
+                    "angle must only be specified when both x and y are specified.",
+                ))
+            }
+            (Some(_), Some(_), None) => {
+                return Err(de::Error::custom(
+                    "angle must be specified when both x and y are specified.",
+                ))
+            }
+        };
+
+        Ok(Guideline {
+            line,
+            name: guideline.name,
+            color: guideline.color,
+            identifier: guideline.identifier,
+        })
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -198,6 +275,41 @@ mod tests {
         assert_de_tokens_error::<Identifier>(
             &[Token::Str("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")],
             "Identifier must be at most 100 characters long.",
+        );
+    }
+
+    #[test]
+    fn guideline_parsing() {
+        let g1 = Guideline {
+            line: Line::Angle { x: 10.0, y: 20.0, degrees: 360.0 },
+            name: Some("hello".to_string()),
+            color: Some(Color { red: 0.0, green: 0.5, blue: 0.0, alpha: 0.5 }),
+            identifier: Some(Identifier("abcABC123".to_string())),
+        };
+        assert_tokens(
+            &g1,
+            &[
+                Token::Struct { name: "RawGuideline", len: 6 },
+                Token::Str("x"),
+                Token::Some,
+                Token::F32(10.0),
+                Token::Str("y"),
+                Token::Some,
+                Token::F32(20.0),
+                Token::Str("angle"),
+                Token::Some,
+                Token::F32(360.0),
+                Token::Str("name"),
+                Token::Some,
+                Token::Str("hello"),
+                Token::Str("color"),
+                Token::Some,
+                Token::Str("0,0.5,0,0.5"),
+                Token::Str("identifier"),
+                Token::Some,
+                Token::Str("abcABC123"),
+                Token::StructEnd,
+            ],
         );
     }
 }
