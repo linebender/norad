46789c4d14ceb87084a85f230b33425445c9737f
Nikolaus Waxweiler <madigens@gmail.com>
2020-06-14 22:59:56 +0100

    Implement v1 Robofab data upgrading and tests

--- src/ufo.rs
+++ src/ufo.rs
@@ -15,7 +15,7 @@ use crate::fontinfo::FontInfo;
 use crate::glyph::{Glyph, GlyphName};
 use crate::layer::Layer;
 use crate::names::NameList;
-use crate::upconversion::upconvert_kerning;
+use crate::upconversion;
 use crate::Error;
 
 static LAYER_CONTENTS_FILE: &str = "layercontents.plist";
@@ -142,10 +142,10 @@ impl Ufo {
             };
 
             let lib_path = path.join(LIB_FILE);
-            let lib = if lib_path.exists() {
+            let mut lib = if lib_path.exists() {
                 // Value::as_dictionary(_mut) will only borrow the data, but we want to own it.
                 // https://github.com/ebarnard/rust-plist/pull/48
-                match plist::Value::from_file(lib_path)? {
+                match plist::Value::from_file(&lib_path)? {
                     plist::Value::Dictionary(dict) => Some(dict),
                     _ => return Err(Error::ExpectedPlistDictionaryError),
                 }
@@ -171,7 +171,7 @@ impl Ufo {
             };
 
             let features_path = path.join(FEATURES_FILE);
-            let features = if features_path.exists() {
+            let mut features = if features_path.exists() {
                 let features = fs::read_to_string(features_path)?;
                 Some(features)
             } else {
@@ -205,11 +205,33 @@ impl Ufo {
                 (_, None, k) => (None, k), // Without a groups.plist, there's nothing to upgrade.
                 (_, Some(g), k) => {
                     let (groups, kerning) =
-                        upconvert_kerning(&g, &k.unwrap_or_default(), &glyph_names);
+                        upconversion::upconvert_kerning(&g, &k.unwrap_or_default(), &glyph_names);
                     validate_groups(&groups).map_err(Error::GroupsUpconversionError)?;
                     (Some(groups), Some(kerning))
                 }
             };
+
+            // The v1 format stores some Postscript hinting related data in the lib,
+            // which we only import into fontinfo if we're reading a v1 UFO.
+            if meta.format_version == FormatVersion::V1 {
+                let mut feature_text = String::new();
+                let mut fontinfo =
+                    if let Some(fontinfo) = font_info { fontinfo } else { FontInfo::default() };
+
+                if let Some(lib_data) = &mut lib {
+                    upconversion::upconvert_ufov1_robofab_data(
+                        &lib_path,
+                        lib_data,
+                        &mut feature_text,
+                        &mut fontinfo,
+                    )?;
+                }
+
+                if !feature_text.is_empty() {
+                    features = Some(feature_text);
+                }
+                font_info = Some(fontinfo);
+            }
 
             meta.format_version = FormatVersion::V3;
 
@@ -407,6 +429,7 @@ fn validate_groups(groups_map: &Groups) -> Result<(), GroupsValidationError> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::shared_types::IntegerOrFloat;
 
     #[test]
     fn new_is_v3() {
@@ -443,6 +466,73 @@ mod tests {
         assert_eq!(font_obj.groups.unwrap().get("public.kern1.@MMK_L_A"), Some(&vec!["A".into()]));
         assert_eq!(font_obj.kerning.unwrap().get("B").unwrap().get("H").unwrap(), &-40.0);
         assert_eq!(font_obj.features.unwrap(), "# this is the feature from lightWide\n");
+    }
+
+    #[test]
+    fn upconvert_ufov1_robofab_data() {
+        let path = "testdata/fontinfotest_v1.ufo";
+        let font = Ufo::load(path).unwrap();
+
+        assert_eq!(font.meta.format_version, FormatVersion::V3);
+
+        let font_info = font.font_info.unwrap();
+        assert_eq!(font_info.postscript_blue_fuzz, Some(IntegerOrFloat::from(1)));
+        assert_eq!(font_info.postscript_blue_scale, Some(0.039625));
+        assert_eq!(font_info.postscript_blue_shift, Some(IntegerOrFloat::from(7)));
+        assert_eq!(
+            font_info.postscript_blue_values,
+            Some(vec![
+                IntegerOrFloat::from(-10),
+                IntegerOrFloat::from(0),
+                IntegerOrFloat::from(482),
+                IntegerOrFloat::from(492),
+                IntegerOrFloat::from(694),
+                IntegerOrFloat::from(704),
+                IntegerOrFloat::from(739),
+                IntegerOrFloat::from(749)
+            ])
+        );
+        assert_eq!(
+            font_info.postscript_other_blues,
+            Some(vec![IntegerOrFloat::from(-260), IntegerOrFloat::from(-250)])
+        );
+        assert_eq!(
+            font_info.postscript_family_blues,
+            Some(vec![IntegerOrFloat::from(500.0), IntegerOrFloat::from(510.0)])
+        );
+        assert_eq!(
+            font_info.postscript_family_other_blues,
+            Some(vec![IntegerOrFloat::from(-260), IntegerOrFloat::from(-250)])
+        );
+        assert_eq!(font_info.postscript_force_bold, Some(true));
+        assert_eq!(
+            font_info.postscript_stem_snap_h,
+            Some(vec![IntegerOrFloat::from(100), IntegerOrFloat::from(120)])
+        );
+        assert_eq!(
+            font_info.postscript_stem_snap_v,
+            Some(vec![IntegerOrFloat::from(80), IntegerOrFloat::from(90)])
+        );
+
+        assert_eq!(
+            font.lib.unwrap().keys().collect::<Vec<&String>>(),
+            vec!["org.robofab.testFontLibData"]
+        );
+
+        assert_eq!(
+            font.features.unwrap(),
+            "@myClass = [A B];\n\nfeature liga {\n    sub A A by b;\n} liga;\n"
+        );
+    }
+
+    #[test]
+    fn upconversion_fontinfo_v123() {
+        let ufo_v1 = Ufo::load("testdata/fontinfotest_v1.ufo").unwrap();
+        let ufo_v2 = Ufo::load("testdata/fontinfotest_v2.ufo").unwrap();
+        let ufo_v3 = Ufo::load("testdata/fontinfotest_v3.ufo").unwrap();
+
+        assert_eq!(ufo_v1, ufo_v3);
+        assert_eq!(ufo_v2, ufo_v3);
     }
 
     #[test]
