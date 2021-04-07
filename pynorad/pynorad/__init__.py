a1576fbed9827076886219b6214425f00aa9048c
Colin Rofls <colin@cmyr.net>
2021-04-07 12:32:50 -0400

    wip

---
+++ pynorad/pynorad/__init__.py
@@ -0,0 +1,4 @@
+class Font(object):
+    """A fontfile"""
+    def __init__(self, path):
+        self.path = str(path)
--- pynorad/src/lib.rs
+++ pynorad/src/lib.rs
@@ -1,7 +1,52 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+
+use std::collections::BTreeMap;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use norad::{MetaInfo, GlyphName, GlifVersion, Guideline, Anchor, Component, Contour};
+
+type SharedObj<T> = Rc<RefCell<T>>;
+
+#[derive(Debug, Clone)]
+struct SharedList<T> {
+    items: Rc<RefCell<T>>,
+}
+
+pub struct PyFont {
+    metainfo: SharedObj<MetaInfo>,
+
+}
+
+// acts like a dictionary of str: layer
+// len()
+// iterator over layers
+// __contains__
+// __get__
+// __del__
+// .defaultLayer
+// newLayer(name, **kwargs) create and return a layer
+// renameGlyph(name, newName, overwrite) rename across all glyphs. if 'overwrite' is false,
+// raises an exception if the new name already exists
+pub struct PyLayerSet {
+    default: PyLayer,
+    layers: SharedList<PyLayer>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PyLayer {
+    glyphs: SharedObj<BTreeMap<GlyphName, SharedObj<PyGlyph>>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PyGlyph {
+    name: GlyphName,
+    format: GlifVersion,
+    height: f32,
+    width: f32,
+    codepoints: Vec<char>,
+    note: SharedObj<String>,
+    guidelines: SharedList<SharedObj<Guideline>>,
+    anchors: SharedList<SharedObj<Anchor>>,
+    components: SharedList<SharedObj<Component>>,
+    contours: SharedList<SharedObj<Contour>>,
 }
