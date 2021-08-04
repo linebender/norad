cc7123d3f858e8dd6b1c972801b080c68f0b3d02
Colin Rofls <colin@cmyr.net>
2021-08-04 15:41:23 -0400

    These were for debugging, committed by mistake

--- examples/load_a_bunch.rs
+++
@@ -1,8 +0,0 @@
-use norad::Font;
-
-fn main() {
-    let _ = Font::load("/Users/rofls/dev/projects/fontville/fontfiles/NotoSans-Bold.ufo").unwrap();
-    let _ =
-        Font::load("/Users/rofls/dev/projects/fontville/fontfiles/NotoSans-Regular.ufo").unwrap();
-    let _ = Font::load("/Users/rofls/dev/projects/fontville/fontfiles/NotoSans-Light.ufo").unwrap();
-}
