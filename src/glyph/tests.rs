8169c76f573742b6d1a3a14fa2e3f48c43c56f33
Chris Simpkins <git.simpkins@gmail.com>
2021-08-14 18:51:58 -0400

    [glyph::tests] add single quote xml decl without custom spacing test

--- src/glyph/tests.rs
+++ src/glyph/tests.rs
@@ -96,6 +96,36 @@ fn serialize_with_custom_whitespace() {
 }
 
 #[test]
+fn serialize_with_single_quote_style() {
+    let data = include_str!("../../testdata/small_lib.glif");
+    let glyph = parse_glyph(data.as_bytes()).unwrap();
+    let options = WriteOptions::default().quote_char(QuoteChar::Single);
+    let one_tab = glyph.encode_xml_with_options(&options).unwrap();
+    let one_tab = std::str::from_utf8(&one_tab).unwrap();
+    pretty_assertions::assert_eq!(
+        one_tab,
+        r#"<?xml version='1.0' encoding='UTF-8'?>
+<glyph name="hello" format="2">
+	<advance width="1200"/>
+	<outline>
+		<contour>
+			<point x="2" y="30" type="line"/>
+			<point x="44" y="10" type="line"/>
+		</contour>
+	</outline>
+	<lib>
+		<dict>
+			<key>test.key</key>
+			<string>I am a creative professional :)</string>
+		</dict>
+	</lib>
+	<note>durp</note>
+</glyph>
+"#
+    );
+}
+
+#[test]
 fn serialize_with_custom_whitespace_and_single_quote_style() {
     let data = include_str!("../../testdata/small_lib.glif");
     let glyph = parse_glyph(data.as_bytes()).unwrap();
