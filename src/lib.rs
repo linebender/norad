6ca0b17d774ae00d3d594c284d99e2d1c49e2a91
Chris Simpkins <git.simpkins@gmail.com>
2021-11-08 14:40:00 -0500

    [lib docs] update basic usage, add license and source sections

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,62 @@
 //!
 //! [ufo]: http://unifiedfontobject.org/versions/ufo3
 //!
-//! # Basic usage:
+//! # Basic Usage
+//!
+//! Instantiate a UFO font object with a [Font] struct like this:
 //!
 //! ```no_run
 //! use norad::Font;
 //!
-//! let path = "RoflsExtraDim.ufo";
+//! let inpath = "RoflsExtraDim.ufo";
 //! let mut font_obj = Font::load(path).expect("failed to load font");
+//! # let layer = font_obj.default_layer();
+//! # let glyph_a = layer.get_glyph("A").expect("missing glyph");
+//! # assert_eq!(glyph_a.name.as_ref(), "A");
+//! # let outpath = "RoflsSemiDim.ufo";
+//! # font_obj.save(outpath);
+//! ```
+//!
+//! The API may be used to access and modify data in the [Font]:
+//!
+//!```no_run
+//! # use norad::Font;
+//! # let inpath = "RoflsExtraDim.ufo";
+//! # let mut font_obj = Font::load(path).expect("failed to load font");
 //! let layer = font_obj.default_layer();
 //! let glyph_a = layer.get_glyph("A").expect("missing glyph");
 //! assert_eq!(glyph_a.name.as_ref(), "A");
+//! # let outpath = "RoflsSemiDim.ufo";
+//! # font_obj.save(outpath);
 //! ```
+//!
+//! Serialize the font to UFO files on disk with the [Font::save] method:
+//!
+//!```no_run
+//! # use norad::Font;
+//! # let inpath = "RoflsExtraDim.ufo";
+//! # let mut font_obj = Font::load(path).expect("failed to load font");
+//! # let layer = font_obj.default_layer();
+//! # let glyph_a = layer.get_glyph("A").expect("missing glyph");
+//! # assert_eq!(glyph_a.name.as_ref(), "A");
+//! let outpath = "RoflsSemiDim.ufo";
+//! font_obj.save(outpath);
+//! ```
+//!
+//! # API Documentation
+//!
+//! Details on the full API for working with UFO fonts are available in these docs.
+//!
+//! # License
+//!
+//! norad is licensed under the [MIT](https://github.com/linebender/norad/blob/master/LICENSE-MIT)
+//! and [Apache v2.0](https://github.com/linebender/norad/blob/master/LICENSE-APACHE) licenses.
+//!
+//! # Source
+//!
+//! Source files are available at <https://github.com/linebender/norad>.
 
+#![warn(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links, unsafe_code)]
 
 #[macro_use]
