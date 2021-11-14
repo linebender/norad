6260447b7e616fd02a17182b5d3a27a0f43161a0
Chris Simpkins <chris@sourcefoundry.org>
2021-11-14 12:27:41 -0500

    [data_request] refactor DataRequest builder methods to return owned values

--- src/data_request.rs
+++ src/data_request.rs
@@ -36,44 +36,44 @@ impl DataRequest {
     }
 
     /// Request that returned UFO data include the glyph layers and points.
-    pub fn layers(&mut self, b: bool) -> &mut Self {
+    pub fn layers(mut self, b: bool) -> Self {
         self.layers = b;
         self
     }
 
     /// Request that returned UFO data include <lib> sections.
-    pub fn lib(&mut self, b: bool) -> &mut Self {
+    pub fn lib(mut self, b: bool) -> Self {
         self.lib = b;
         self
     }
 
     /// Request that returned UFO data include parsed `groups.plist`.
-    pub fn groups(&mut self, b: bool) -> &mut Self {
+    pub fn groups(mut self, b: bool) -> Self {
         self.groups = b;
         self
     }
 
     /// Request that returned UFO data include parsed `kerning.plist`.
-    pub fn kerning(&mut self, b: bool) -> &mut Self {
+    pub fn kerning(mut self, b: bool) -> Self {
         self.kerning = b;
         self
     }
 
     /// Request that returned UFO data include OpenType Layout features in Adobe
     /// .fea format.
-    pub fn features(&mut self, b: bool) -> &mut Self {
+    pub fn features(mut self, b: bool) -> Self {
         self.features = b;
         self
     }
 
     /// Request that returned UFO data include data.
-    pub fn data(&mut self, b: bool) -> &mut Self {
+    pub fn data(mut self, b: bool) -> Self {
         self.data = b;
         self
     }
 
     /// Request that returned UFO data include images.
-    pub fn images(&mut self, b: bool) -> &mut Self {
+    pub fn images(mut self, b: bool) -> Self {
         self.images = b;
         self
     }
@@ -82,5 +82,60 @@ impl DataRequest {
 impl Default for DataRequest {
     fn default() -> Self {
         DataRequest::from_bool(true)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn all_fields_are_true(dr: &DataRequest) -> bool {
+        return dr.layers
+            && dr.lib
+            && dr.groups
+            && dr.kerning
+            && dr.features
+            && dr.data
+            && dr.images;
+    }
+
+    fn all_fields_are_false(dr: &DataRequest) -> bool {
+        return !dr.layers
+            && !dr.lib
+            && !dr.groups
+            && !dr.kerning
+            && !dr.features
+            && !dr.data
+            && !dr.images;
+    }
+
+    #[test]
+    fn test_datarequest_default() {
+        assert!(all_fields_are_true(&DataRequest::default()));
+    }
+
+    #[test]
+    fn test_datarequest_all() {
+        assert!(all_fields_are_true(&DataRequest::all()));
+    }
+
+    #[test]
+    fn test_datarequest_none() {
+        assert!(all_fields_are_false(&DataRequest::none()));
+    }
+
+    #[test]
+    fn test_datarequest_builder() {
+        let dr = DataRequest::default()
+            .layers(false)
+            .lib(false)
+            .groups(false)
+            .kerning(false)
+            .features(false)
+            .data(false)
+            .images(false);
+
+        assert!(all_fields_are_false(&dr));
     }
 }
